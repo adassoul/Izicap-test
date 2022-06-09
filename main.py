888e60859c454d039899aa83d75ae2512867e4c2
adassoul <aymane.dassouli@gmail.com>
2022-06-09 18:51:46 +0200

    ko & ok tests outpud method + a white test

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+from time import sleep
+from selenium import webdriver
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+waiting_period = 20
+
+def ok_message(message):
+    print(f"------Okay!------ : {message} passed")
+
+def ko_message(message):
+    print(f"------Knocked Out------ : {message} didn't pass...")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def verify_title(browser):
+    test_goal = "title verif"
+    title = browser.title
+    try:
+        assert "App title" in title
+        ok_message(test_goal)
+    except AssertionError:
+        ko_message(test_goal)
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    with webdriver.Chrome() as browser:
+        browser.get("https://ismaestro.github.io/angular-example-app/")
+        verify_title(browser)
+        sleep(waiting_period)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
